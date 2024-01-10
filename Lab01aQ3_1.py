30aadef5e348b2f60adfc0c20d9f679b920a9e15
FinnVAL <115410106+FinnVAL@users.noreply.github.com>
2024-01-10 23:16:19 +0700

    Add files via upload

---
+++ Lab01aQ3_1.py
@@ -0,0 +1,7 @@
+try:
+    number1 = float(input("Nhập vào một số thực: "))
+    number2 = float(input("Nhập vào một số thực: "))
+    print("Tích của 2 số đó là: ", number1*number2)
+    print("Tổng của 2 số đó là: ", number1+number2)
+except:
+    print("Hãy nhập vào số thực!")
